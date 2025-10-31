5028ffd9a1cd4aa36e0fcdc9db064187608d9d75
yushixh <yushixh@gmail.com>
2025-10-31 09:06:55 +0000

    Init

---
+++ app.js
@@ -0,0 +1,46 @@
+// Import Express.js
+const { json } = require('body-parser');
+const express = require('express');
+
+// Create an Express app
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Set port and verify_token
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
+
+// Route for GET requests
+app.get('/', (req, res) => {
+  console.log(`\n=== Receive GET request ===\n`)
+  console.log(JSON.stringify(req))
+  console.log(`\n\n`)
+
+  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
+
+  if (mode === 'subscribe' && token === verifyToken) {
+    console.log('WEBHOOK VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.status(403).end();
+  }
+});
+
+// Route for POST requests
+app.post('/', (req, res) => {
+  console.log(`\n=== Receive POST request ===\n`)
+  console.log(JSON.stringify(req))
+  console.log(`\n\n`)
+
+  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+  console.log(`\n\nWebhook received ${timestamp}\n`);
+  console.log(JSON.stringify(req.body, null, 2));
+  res.status(200).end();
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`\nListening on port ${port}\n`);
+});
