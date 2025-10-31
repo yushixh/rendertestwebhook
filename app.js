2928fede5d3aad24528097787129190855749929
yushixh <yushixh@gmail.com>
2025-10-31 17:23:54 +0800

    remove log

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const verifyToken = process.env.VERIFY_TOKEN;
 
 // Route for GET requests
 app.get('/', (req, res) => {
-  console.log(`\n=== Receive GET request ===\n`)
-  console.log(JSON.stringify(req))
-  console.log(`\n\n`)
+  //console.log(`\n=== Receive GET request ===\n`)
+  //console.log(JSON.stringify(req))
+  //console.log(`\n\n`)
 
   const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
 
@@ -30,9 +30,9 @@ app.get('/', (req, res) => {
 
 // Route for POST requests
 app.post('/', (req, res) => {
-  console.log(`\n=== Receive POST request ===\n`)
-  console.log(JSON.stringify(req))
-  console.log(`\n\n`)
+  //console.log(`\n=== Receive POST request ===\n`)
+  //console.log(JSON.stringify(req))
+  //console.log(`\n\n`)
 
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
